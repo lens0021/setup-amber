5237a988e7ef037769ee3175be15a2e57d12adc4
lens0021 <lorentz0021@gmail.com>
2026-02-25 17:21:48 +0900

    ci(tofu): Manage Github settings as code

---
+++ .github/tf/repository.tf
@@ -0,0 +1,47 @@
+import {
+  id = "setup-amber"
+  to = github_repository.this
+}
+resource "github_repository" "this" {
+  allow_auto_merge            = true
+  allow_merge_commit          = false
+  allow_rebase_merge          = false
+  allow_squash_merge          = true
+  allow_update_branch         = true
+  archived                    = false
+  archive_on_destroy          = true
+  auto_init                   = false
+  delete_branch_on_merge      = true
+  description                 = "Download amber compiler"
+  has_discussions             = false
+  has_issues                  = true
+  has_projects                = false
+  has_wiki                    = false
+  homepage_url                = ""
+  name                        = "setup-amber"
+  squash_merge_commit_message = "BLANK"
+  squash_merge_commit_title   = "PR_TITLE"
+  topics                      = []
+  visibility                  = "public"
+  web_commit_signoff_required = false
+
+  security_and_analysis {
+    secret_scanning {
+      status = "enabled"
+    }
+    secret_scanning_push_protection {
+      status = "enabled"
+    }
+  }
+}
+
+# secrets.GITHUB_TOKEN does not have the permission for this:
+# import {
+#   id = "setup-amber"
+#   to = github_workflow_repository_permissions.this
+# }
+# resource "github_workflow_repository_permissions" "this" {
+#   repository                       = github_repository.this.name
+#   default_workflow_permissions     = "read"
+#   can_approve_pull_request_reviews = true
+# }
---
+++ .github/tf/ruleset.tf
@@ -0,0 +1,65 @@
+import {
+  id = "setup-amber:10629749"
+  to = github_repository_ruleset.default
+}
+resource "github_repository_ruleset" "default" {
+  name        = "default"
+  repository  = github_repository.this.name
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["~DEFAULT_BRANCH", ]
+      exclude = []
+    }
+  }
+
+  bypass_actors {
+    actor_id    = 2
+    actor_type  = "RepositoryRole"
+    bypass_mode = "always"
+  }
+
+  rules {
+    deletion                = true
+    non_fast_forward        = true
+    required_linear_history = true
+    required_signatures     = false
+    update                  = true
+
+    pull_request {
+      dismiss_stale_reviews_on_push     = false
+      require_code_owner_review         = false
+      require_last_push_approval        = false
+      required_approving_review_count   = 0
+      required_review_thread_resolution = false
+    }
+
+    required_status_checks {
+      do_not_enforce_on_create             = false
+      strict_required_status_checks_policy = false
+
+      dynamic "required_check" {
+        for_each = [
+          "biome",
+          "check-dist",
+          "--> Linted: CHECKOV",
+          "--> Linted: GITHUB_ACTIONS",
+          "--> Linted: GITLEAKS",
+          "--> Linted: GIT_MERGE_CONFLICT_MARKERS",
+          "--> Linted: JSCPD",
+          "rumdl",
+          "Test with default settings (macos-latest)",
+          "Test with default settings (ubuntu-latest)",
+          "zizmor",
+        ]
+        content {
+          context        = required_check.value
+          integration_id = 15368
+        }
+      }
+    }
+
+  }
+}
